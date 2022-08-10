2be1464e232aaeacb9c0afc0a89bd77c61393593
Mori Keli <kelimbth@gmail.com>
2022-08-10 11:40:28 +0300

    updated the interface

--- staffaccounts/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Librarian)
-admin.site.register(Books)
-admin.site.register(Student)
--- staffaccounts/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StaffaccountsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'staffaccounts'
